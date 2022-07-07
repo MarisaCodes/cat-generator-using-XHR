65b1e9cd0f1b23501a115d1f1b957ed99a64f0f2
MarisaCodes <marisakirisame955@gmail.com>
2022-07-08 02:39:43 +0400

    initial and final commit

---
+++ app.js
@@ -0,0 +1,66 @@
+const btn = document.querySelector(".btn");
+const container = document.querySelector(".container");
+
+// btn.addEventListener("click", searchCats);
+
+// function searchCats(event) {
+//   fetch("https://api.thecatapi.com/v1/images/search")
+//     .then((response) => response.json())
+//     .then((data) => {
+//       let arrContainer = Array.from(event.target.parentElement.children);
+//       arrContainer.forEach((element) => {
+//         if (element.classList.contains("cats")) {
+//           element.remove();
+//         }
+//       });
+//       let catUrl = data[0].url;
+//       let strUrl = catUrl.toString();
+//       let catDiv = document.createElement("div");
+//       let catImg = document.createElement("img");
+//       catDiv.className = "cats";
+//       catImg.className = "cat-gen-img";
+//       catImg.setAttribute("src", strUrl);
+//       catDiv.appendChild(catImg);
+//       container.appendChild(catDiv);
+//     })
+//     .catch((err) => console.log(err));
+// }
+
+function getCatimg(callback) {
+  const request = new XMLHttpRequest();
+
+  request.open("GET", "https://api.thecatapi.com/v1/images/search");
+  request.send();
+
+  request.addEventListener("readystatechange", () => {
+    if (request.readyState === 4 && request.status === 200) {
+      let data = JSON.parse(request.responseText);
+      let catUrl = data[0].url.toString();
+      callback(undefined, catUrl);
+    } else if (request.readyState === 4) {
+      callback("could not fetch data", undefined);
+    }
+  });
+}
+
+btn.addEventListener("click", (event) => {
+  getCatimg((err, url) => {
+    if (err) {
+      console.log(err);
+    } else {
+      let arrContainer = Array.from(event.target.parentElement.children);
+      arrContainer.forEach((element) => {
+        if (element.classList.contains("cats")) {
+          element.remove();
+        }
+      });
+      let catDiv = document.createElement("div");
+      let catImg = document.createElement("img");
+      catDiv.className = "cats";
+      catImg.className = "cat-gen-img";
+      catImg.setAttribute("src", url);
+      catDiv.appendChild(catImg);
+      container.appendChild(catDiv);
+    }
+  });
+});
